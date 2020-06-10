13d19a5eb2898fc1ad2f7a8d752959aa95f09fa7
Delta-dev-99 <delta.deltadev.127@gmail.com>
2020-06-10 16:12:17 -0400

    Added functions to print time Added a file to .gitignore

--- Proyecto numero 3 de windows desktop.cpp
+++ Proyecto numero 3 de windows desktop.cpp
@@ -2,10 +2,50 @@
 //
 
 #include <iostream>
+#include <chrono>
+#include <iomanip>
+
+
+//Function to print points in time
+template <typename ClockType, typename Duration>
+std::ostream &operator<<(std::ostream &output_stream, const std::chrono::time_point<ClockType, Duration> &time_point)
+{
+    auto ttime = ClockType::to_time_t(time_point);
+
+    char buffer[26];
+    ctime_r(&ttime, buffer);
+    buffer[24] = 0; //Remove newline that is added
+
+    return output_stream << buffer;
+}
+
+//Function to print durations (time lapses) with nanosecond precision
+template <typename Rep, typename Period>
+std::ostream & operator <<(std::ostream &output_stream, const std::chrono::duration<Rep, Period> &timeSpan)
+{
+    auto hours = std::chrono::duration_cast<std::chrono::hours>(timeSpan);
+	auto minutes = (std::chrono::duration_cast<std::chrono::minutes>(timeSpan) % std::chrono::hours{1});
+	auto seconds = (std::chrono::duration_cast<std::chrono::seconds>(timeSpan) % std::chrono::minutes{1});
+	auto nanos = timeSpan - hours - minutes - seconds;
+
+    return output_stream << std::setw(2) << std::setfill('0') << hours.count() << ":" << std::setw(2) << minutes.count() << "." << std::setw(2) << seconds.count() << " + " << nanos.count() << " nanos";
+}
+
+
 
 int main()
 {
+    auto startTime = std::chrono::high_resolution_clock::now();
+
     std::cout << "Hello World!\n";
     std::cout << "Another Hello World! :)\n";
-}
 
+    std::cout << std::chrono::system_clock::now() << "\n";
+
+    auto endTime = std::chrono::high_resolution_clock::now();
+
+    //substracting 2 time points yields a duration
+    std::cout << "Execution of program took about " << (endTime - startTime) << "\n";
+
+    std::cout << "\n";
+}
